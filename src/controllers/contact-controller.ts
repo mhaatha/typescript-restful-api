4829bac3b01f5062777868e4c6dafc036bb3d45b
mhaatha <mhafidza528@gmail.com>
2024-05-09 17:58:42 +0800

    Search Contact API

--- src/controllers/contact-controller.ts
+++ src/controllers/contact-controller.ts
@@ -1,6 +1,6 @@
 import { Response, NextFunction } from "express";
 import { UserRequest } from "../types/user-request";
-import { CreateContactRequest, UpdateContactRequest } from "../models/contact-model";
+import { CreateContactRequest, SearchContactRequest, UpdateContactRequest } from "../models/contact-model";
 import { ContactService } from "../services/contact-service";
 
 export class ContactController {
@@ -53,5 +53,20 @@ export class ContactController {
       next(e);
     }
   }
-
+  
+  static async search(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const request: SearchContactRequest = {
+        name: req.query.name as string,
+        email: req.query.email as string,
+        phone: req.query.phone as string,
+        page: req.query.page? Number(req.query.page): 1,
+        size: req.query.size? Number(req.query.size): 10
+      }
+      const response = await ContactService.search(req.user!, request);
+      res.status(200).json(response);
+    } catch (e) {
+      next(e);
+    }
+  }
 }
--- src/services/contact-service.ts
+++ src/services/contact-service.ts
@@ -1,9 +1,10 @@
-import { ContactResponse, CreateContactRequest, UpdateContactRequest, toContactResponse } from "../models/contact-model";
+import { ContactResponse, CreateContactRequest, SearchContactRequest, UpdateContactRequest, toContactResponse } from "../models/contact-model";
 import { Validation } from "../validations/validation";
 import { ContactValidation } from "../validations/contact-validation";
 import { User, Contact } from "@prisma/client";
 import { prismaClient } from "../applications/database";
 import { ResponseError } from "../errors/response-error";
+import { Pageable } from "../models/page";
 
 export class ContactService {
   static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
@@ -67,4 +68,71 @@ export class ContactService {
 
     return toContactResponse(contact);
   }
+
+  static async search(user: User, request: SearchContactRequest): Promise<Pageable<ContactResponse>> {
+    const searchRequest = Validation.validate(ContactValidation.SEARCH, request);
+    const skip = (searchRequest.page - 1) * searchRequest.size;
+
+    // Dynamic query
+    const filters = [];
+    // Check if name exists
+    if (searchRequest.name) {
+      filters.push({
+        OR: [
+          {
+            first_name: {
+              contains: searchRequest.name
+            }
+          },
+          {
+            last_name: {
+              contains: searchRequest.name
+            }
+          }
+        ]
+      })
+    }
+    // Check if email exists
+    if (searchRequest.email) {
+      filters.push({
+        email: {
+          contains: searchRequest.email
+        }
+      })
+    }
+    // Check if phone exists
+    if (searchRequest.phone) {
+      filters.push({
+        phone: {
+          contains: searchRequest.phone
+        }
+      })
+    }
+
+    const contacts = await prismaClient.contact.findMany({
+      where: {
+        username: user.username,
+        AND: filters
+      },
+      take: searchRequest.size,
+      skip: skip
+    });
+
+    const total = await prismaClient.contact.count({
+      where: {
+        username: user.username,
+        AND: filters
+      }
+    });
+
+    return {
+      data: contacts.map(contact => toContactResponse(contact)),
+      paging: {
+        current_page: searchRequest.page,
+        total_page: Math.ceil(total / searchRequest.size),
+        size: searchRequest.size
+      }
+    }
+  }
 }
+
