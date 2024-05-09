68efa25c4f0cd37ef7a2802b2eb79c13fc3a4f45
mhaatha <mhafidza528@gmail.com>
2024-05-09 15:27:19 +0800

    Get Contact API

--- src/services/contact-service.ts
+++ src/services/contact-service.ts
@@ -3,6 +3,7 @@ import { Validation } from "../validations/validation";
 import { ContactValidation } from "../validations/contact-validation";
 import { User } from "@prisma/client";
 import { prismaClient } from "../applications/database";
+import { ResponseError } from "../errors/response-error";
 
 export class ContactService {
   static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
@@ -14,6 +15,21 @@ export class ContactService {
         username: user.username
       }
     });
+
+    return toContactResponse(contact);
+  }
+
+  static async get(user: User, id: number): Promise<ContactResponse> {
+    const contact = await prismaClient.contact.findUnique({
+      where: {
+        id: id,
+        username: user.username
+      }
+    });
+
+    if (!contact) {
+      throw new ResponseError(404, "Contact not found");
+    }
 
     return toContactResponse(contact);
   }
