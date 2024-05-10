149c69e25e5cc9068ff46df855deb6d7f788c1f7
mhaatha <mhafidza528@gmail.com>
2024-05-10 15:07:34 +0800

    Get Address API

--- src/controllers/address-controller.ts
+++ src/controllers/address-controller.ts
@@ -1,4 +1,4 @@
-import { CreateAddressRequest } from "../models/address-model";
+import { CreateAddressRequest, GetAddressRequest } from "../models/address-model";
 import { AddressService } from "../services/address-service";
 import { UserRequest } from "../types/user-request";
 import { Response, NextFunction } from "express";
@@ -10,6 +10,22 @@ export class AddressController {
       request.contact_id = Number(req.params.contactId);
 
       const response = await AddressService.create(req.user!, request);
+      res.status(200).json({
+        data: response
+      });
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  static async get(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const request: GetAddressRequest = {
+        id: Number(req.params.addressId),
+        contact_id: Number(req.params.contactId)
+      };
+
+      const response = await AddressService.get(req.user!, request);
       res.status(200).json({
         data: response
       });
--- src/services/address-service.ts
+++ src/services/address-service.ts
@@ -1,9 +1,10 @@
 import { User } from "@prisma/client";
-import { AddressResponse, CreateAddressRequest, toAddressResponse } from "../models/address-model";
+import { AddressResponse, CreateAddressRequest, GetAddressRequest, toAddressResponse } from "../models/address-model";
 import { Validation } from "../validations/validation";
 import { AddressValidation } from "../validations/address-validation";
 import { ContactService } from "./contact-service";
 import { prismaClient } from "../applications/database";
+import { ResponseError } from "../errors/response-error";
 
 export class AddressService {
   static async create(user: User, request: CreateAddressRequest): Promise<AddressResponse> {
@@ -13,6 +14,24 @@ export class AddressService {
     const address = await prismaClient.address.create({
       data: createRequest
     });
+
+    return toAddressResponse(address);
+  }
+
+  static async get(user: User, request: GetAddressRequest): Promise<AddressResponse> {
+    const getRequest = Validation.validate(AddressValidation.GET, request);
+    await ContactService.checkContactMustExists(user.username, getRequest.contact_id);
+
+    const address = await prismaClient.address.findFirst({
+      where: {
+        id: getRequest.id,
+        contact_id: getRequest.contact_id
+      }
+    });
+
+    if (!address) {
+      throw new ResponseError(404, "Address not found");
+    }
 
     return toAddressResponse(address);
   }
